4f976cf4c92c25ab998ac65b3596835b8126cc5c
Withyen <wijayakumaranwithyen@gmail.com>
2025-09-26 21:32:56 +0530

    feat: add music player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Sidebar from './componets/Sidebar'
+import Player from './componets/Player'
 
 const App = () => {
   return (
   <div className='h-screen bg-black'>
     <div className='h-[90%] flex'>
       <Sidebar/>
-
     </div>
-      
+      <Player/>
     </div>
   )
 }
