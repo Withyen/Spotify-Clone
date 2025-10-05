e129c3d30205f2fe0126bc2f7040b5f4ca71b8fa
Withyen Wijiyakumaran <wijayakumaranwithyen@gmail.com>
2025-10-05 23:47:42 +0530

    feat: add album list and song list components

--- src/componets/DisplayHome.jsx
+++ src/componets/DisplayHome.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
-import Albumitem from './Albumitem'
+import AlbumItem from './Albumitem'
+import { songsData } from '../assets/assets'
+import SongItem from './SongItem'
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar/>
-      <div className='my-5 font-bold text-2xl'>
-        {albumsData.map((item,index) => (<Albumitem key={index} image={item.image} name={item.name} desc={item.desc} id={item.id}/>))}
+      <div className='mb-4'>
+        <h1 className='text-2xl font-bold mb-5'>Featured charts</h1>
+        <div className='flex overflow-auto'>
+          {albumsData.map((item,index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>
       </div>
+      <div className='mb-4'>
+        <h1 className='text-2xl font-bold mb-5'>Today's biggest hits</h1>
+        <div className='flex overflow-auto'>
+          {songsData.map((item,index) => (<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>
+      </div>  
     </>
   )
 }
