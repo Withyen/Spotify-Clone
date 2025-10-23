7063e3164a059b124cabfa4e1d52be849a2c0c98
Withyen Wijiyakumaran <wijayakumaranwithyen@gmail.com>
2025-10-23 22:26:10 +0530

    feat: added play with id functionality

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -32,10 +32,17 @@ const PlayerContextProvider = (props) => {
         setPlayerStatus(false);
     }
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayerStatus(true);
+    }
+
     useEffect(() => {
         setTimeout(() => {
 
             audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
                 setTime({
                     currentTime: {
                         second: Math.floor(audioRef.current.currentTime % 60),
@@ -57,7 +64,8 @@ const PlayerContextProvider = (props) => {
         track, setTrack,
         playerStatus, setPlayerStatus,
         time, setTime,
-        play, pause
+        play, pause,
+        playWithId
     }
 
     return (
